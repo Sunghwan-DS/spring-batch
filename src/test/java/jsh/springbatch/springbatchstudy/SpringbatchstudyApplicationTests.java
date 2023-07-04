11a79f76e2ffb8899f63bdba551ce9aa5588e726
sunghwan-ds <jeonsung02@gmail.com>
2023-07-04 21:24:17 +0900

    Create spring-batch Project

---
+++ src/test/java/jsh/springbatch/springbatchstudy/SpringbatchstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.springbatch.springbatchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
