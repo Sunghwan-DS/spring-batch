6f4924c20f174cef9a4938017beeb8f3826686b8
sunghwan-ds <jeonsung02@gmail.com>
2023-07-04 21:36:06 +0900

    스프링 배치가 작동하기 위한 어노테이션 선언 - 총 4개의 설정 클래스를 실행시키며 스프링 배치의 모든 초기화 및 실행 구성이 이루어진다 - 스프링 부트 배치의 자동 설정 클래스가 실행됨으로 빈으로 등록된 모든 Job을 검색해서 초기화와 동시에 Job을 수행하도록 구성됨

--- src/main/java/jsh/springbatch/springbatchstudy/SpringbatchstudyApplication.java
+++ src/main/java/jsh/springbatch/springbatchstudy/SpringbatchstudyApplication.java
@@ -1,9 +1,11 @@
 package jsh.springbatch.springbatchstudy;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringbatchstudyApplication {
 
 	public static void main(String[] args) {
