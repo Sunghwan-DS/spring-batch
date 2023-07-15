42823c470b6bd5afa055760441a95323560a839a
Sunghwan-DS <jeonsung02@gmail.com>
2023-07-15 17:25:35 +0900

    JobInstance 예제 소스 추가 - job name과 job parameter를 BATCH_JOB_EXECUTION_PARAMS 테이블에 적재

---
+++ src/main/java/jsh/springbatch/springbatchstudy/JobInstanceConfiguration.java
@@ -0,0 +1,55 @@
+package jsh.springbatch.springbatchstudy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobInstanceConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job instanceJob() {
+        return jobBuilderFactory.get("instanceJob")
+                                .start(instanceStep1())
+                                .next(instanceStep2())
+                                .build();
+    }
+
+    @Bean
+    public Step instanceStep1() {
+        return stepBuilderFactory.get("instanceStep1")
+                                 .tasklet(new Tasklet() {
+                                     @Override
+                                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                                         System.out.println("instanceStep1 was excuted");
+                                         return null;
+                                     }
+                                 })
+                                 .build();
+    }
+
+    @Bean
+    public Step instanceStep2() {
+        return stepBuilderFactory.get("instanceStep2")
+                                 .tasklet(new Tasklet() {
+                                     @Override
+                                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                                         System.out.println("instanceStep2 was excuted");
+                                         return null;
+                                     }
+                                 })
+                                 .build();
+    }
+}
