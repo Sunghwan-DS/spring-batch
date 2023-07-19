b8916debd01ed64faba38a8b37a88ff8bff1c530
Sunghwan-DS <jeonsung02@gmail.com>
2023-07-19 16:39:52 +0900

    Step 예제 소스 추가 - CustomTasklet 사용

---
+++ src/main/java/jsh/springbatch/springbatchstudy/StepConfiguration.java
@@ -0,0 +1,46 @@
+package jsh.springbatch.springbatchstudy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class StepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job stepJob() {
+        return jobBuilderFactory.get("stepJob")
+                                .start(stepJobStep1())
+                                .next(stepJobStep2())
+                                .build();
+    }
+
+    @Bean
+    public Step stepJobStep1() {
+        return stepBuilderFactory.get("stepJobStep1")
+                                 .tasklet(new CustomTasklet())
+                                 .build();
+    }
+
+    @Bean
+    public Step stepJobStep2() {
+        return stepBuilderFactory.get("stepJobStep2")
+                                 .tasklet((stepContribution, chunkContext) -> {
+                                     System.out.println("stepJobStep2 has executed");
+                                     return RepeatStatus.FINISHED;
+                                 })
+                                 .build();
+    }
+}
